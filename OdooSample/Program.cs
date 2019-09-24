c7906de32bf84329d226d4703e352956163bb2d7
ismail eski <ismaile@projetgrup.com>
2019-09-24 10:15:09 +0300

    initial

---
+++ OdooSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Odoo.Concrete;
+using System;
+
+namespace OdooSample
+{
+    class Program
+    {
+        private static readonly IServiceProvider ServiceProvider;
+
+        static void Main(string[] args)
+        {
+          
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var rpcConnnectionSettings = new RpcConnectionSetting();
+            config.GetSection("OdooConnection").Bind(rpcConnnectionSettings);
+
+
+            var odooConn = new RpcConnection(rpcConnnectionSettings);
+            var rpcContext = new RpcContext(odooConn, "res.partner");
+
+            rpcContext.RpcFilter
+                .Equal("id", 10);
+
+            rpcContext
+                .AddField("id")
+                .AddField("name");
+
+            var data = rpcContext.Execute(true);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
